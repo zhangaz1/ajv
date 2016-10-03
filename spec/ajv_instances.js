dc1a968288514e9132d51fb75642027d9b6cb1be
Evgeny Poberezkin <e.poberezkin@me.com>
2016-10-03 14:01:26 +0100

    test: portable validation functions created by ajv-pack

--- spec/ajv_instances.js
+++ spec/ajv_instances.js
@@ -4,9 +4,20 @@ var Ajv = require('./ajv');
 
 module.exports = getAjvInstances;
 
+var isBrowser = typeof window == 'object';
+var packTest = !isBrowser && process.env.AJV_PACK == 'true';
+
 
 function getAjvInstances(options, extraOpts) {
-  return _getAjvInstances(options, extraOpts || {});
+  var instances = _getAjvInstances(options, extraOpts || {});
+  if (!packTest) return instances;
+  var ajvPack = require('' + 'ajv-pack');
+  extraOpts = extraOpts || {};
+  extraOpts.sourceCode = true;
+  var packInstances = _getAjvInstances(options, extraOpts).map(function (ajv) {
+    return ajvPack.instance(ajv);
+  });
+  return instances.concat(packInstances);
 }
 
 function _getAjvInstances(opts, useOpts) {
